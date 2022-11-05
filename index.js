0fb914911757115b1a5e375591a47dcd1fd2feb1
madrodgerflynn <madrodgerflynn@gmail.com>
2022-11-05 14:45:24 -0600

    beginning algos

---
+++ index.js
@@ -0,0 +1,8 @@
+var isSquare = function (n) {
+  //   const number = 55;
+  //   if (number.length / 2 == true && number.length > 0) {
+  //     return n;
+  //   }
+  return n >= 0 && Math.sqrt(n) % 1 === 0;
+  console.log(number);
+};
