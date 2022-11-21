4d5c849d567ed306ba8c1345aeae789d5d929a7d
madrodgerflynn <madrodgerflynn@gmail.com>
2022-11-21 11:07:10 -0700

    fizzbuzz

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-var isSquare = function (n) {
-  //   const number = 55;
-  //   if (number.length / 2 == true && number.length > 0) {
-  //     return n;
-  //   }
-  return n >= 0 && Math.sqrt(n) % 1 === 0;
-  console.log(number);
-};
+// Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+// Replace certain values however if any of the following conditions are met:
+
+// If the value is a multiple of 3: use the value "Fizz" instead
+// If the value is a multiple of 5: use the value "Buzz" instead
+// If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+// N will never be less than 1.
+
+// Return an array
+function fizzbuzz(n) {
+  let i = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // for (var n = 1; n < 101; n++)
+  if (i % 15 == 0) {
+    return console.log("fizzBuzz");
+  } else if (i % 3 == 0) {
+    return console.log("Fizz");
+  } else if (i % 5 == 0) {
+    return console.log("Buzz");
+  } else {
+    return console.log(i);
+  }
+}
